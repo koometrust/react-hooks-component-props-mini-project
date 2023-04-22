ec04f581c1b2d139b8bb123b98accac712707e66
Koome <judekoome360@gmail.com>
2023-04-22 15:32:31 +0300

    Completed Assignments

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,27 +1,27 @@
 import logo from "../assets/logo.svg";
 
 const blogData = {
-  name: "Underreacted",
+  name: "Like!?",
   image: logo,
   about: "A blog about learning React",
   posts: [
     {
       id: 1,
-      title: "Components 101",
+      title: "korb$",
       date: "December 15, 2020",
       preview: "Setting up the building blocks of your site",
       minutes: 5,
     },
     {
       id: 2,
-      title: "React Data Flow",
+      title: "$abi Wu",
       date: "December 11, 2020",
       preview: "Passing props is never passé",
       minutes: 15,
     },
     {
       id: 3,
-      title: "Function vs Class Components",
+      title: "Grahhhh",
       preview: "React, meet OOJS.",
       minutes: 47,
     },
